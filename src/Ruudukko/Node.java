1f8f6342cf1619387bb623529faaff4ec82f39ed
hkskogbe <heikki.skogberg@gmail.com>
2013-09-19 13:01:21 +0300

    Hienosaadetty algoritmia eteenpain

--- src/Ruudukko/Node.java
+++ src/Ruudukko/Node.java
@@ -6,11 +6,6 @@ public class Node {
     private NodeSet tyyppi;
     
     private int gScore;
-    private int fScore;
-
-    public void setfScore(int fScore) {
-        this.fScore = fScore;
-    }
 
     public void setgScore(int gScore) {
         this.gScore = gScore;
@@ -20,14 +15,10 @@ public class Node {
         return gScore;
     }
 
-    public int getfScore() {
-        return fScore;
-    }
 
     public Node() {
         this.parent = null;
         this.tyyppi = NodeSet.NONE;
-        this.fScore = Integer.MAX_VALUE;
         this.gScore = Integer.MAX_VALUE;
     }
 
